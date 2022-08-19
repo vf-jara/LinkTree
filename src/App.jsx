401d6e001366c926c61e75bff91e2cae0f4311b9
Vinicius Jara <viniciusfjara@gmail.com>
2022-08-19 01:54:42 -0300

    inicia animações

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
     <div className='bg-sky-100 w-full'>
       <div className='flex flex-col mx-auto min-h-screen max-w-[500px] items-center py-10 '>
         <div>
-          <img src="https://github.com/vf-jara.png" className='rounded-full p-[2px] border-4 border-sky-700 h-[150px]'/>
+          <img src="https://github.com/vf-jara.png" className='rounded-full p-[2px] border-4 border-sky-700 h-[150px]' />
         </div>
         <div className='py-5 text-center'>
           <h1 className='text-2xl font-bold'>Vinicius Franco Jara</h1>
           <p>Desenvolvedor Front-End<br />JavaScript | React | Next.js</p>
         </div>
         <div className='w-full flex flex-col gap-5 px-10'>
-          <a href='https://github.com/vf-jara' target="_blank" className='font-medium text-xl'>
+          <a href='https://github.com/vf-jara' target="_blank" className='card font-medium text-xl'>
             <div className='bg-gray-800 text-white flex rounded-md'>
               <div className=' px-5 py-3'>
                 <GithubLogo size={35} />
@@ -24,7 +24,7 @@ function App() {
               </div>
             </div>
           </a>
-          <a href='https://www.linkedin.com/in/vfjara21/' target="_blank" className='font-medium text-xl'>
+          <a href='https://www.linkedin.com/in/vfjara21/' target="_blank" className='card2 delay-75 font-medium text-xl'>
             <div className='bg-[#0a66c2] text-white flex rounded-md'>
               <div className=' px-5 py-3'>
                 <LinkedinLogo size={35} />
@@ -34,7 +34,7 @@ function App() {
               </div>
             </div>
           </a>
-          <a href='mailto:viniciusfjara@gmail.com' target="_blank" className='font-medium text-xl'>
+          <a href='mailto:viniciusfjara@gmail.com' target="_blank" className='card3 delay-150 font-medium text-xl'>
             <div className='bg-[#c5221f] text-white flex rounded-md'>
               <div className=' px-5 py-3'>
                 <Envelope size={35} />
