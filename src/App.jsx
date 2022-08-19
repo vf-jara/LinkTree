a2f4f72874be3f4e2503e5c01c40fb05fb5e654d
Vinicius Jara <viniciusfjara@gmail.com>
2022-08-19 02:03:50 -0300

    adiciona imagem fixa

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className='bg-sky-100 w-full'>
       <div className='flex flex-col mx-auto min-h-screen max-w-[500px] items-center py-10 '>
         <div className='fade'>
-          <img src="https://github.com/vf-jara.png" className='rounded-full p-[2px] border-4 border-sky-700 h-[150px]' />
+          <img src="/image.jpg" className='rounded-full p-[2px] border-4 border-sky-700 h-[150px]' />
         </div>
         <div className='py-5 text-center'>
           <h1 className='text-2xl font-bold'>Vinicius Franco Jara</h1>
