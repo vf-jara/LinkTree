f34cae6829f60f3a1aea3d7e60d35cf7c8abb296
Vinicius Jara <viniciusfjara@gmail.com>
2022-08-19 00:25:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { GithubLogo, LinkedinLogo, Envelope } from 'phosphor-react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-sky-100 w-full'>
+      <div className='flex flex-col mx-auto min-h-screen max-w-[500px] items-center py-10 '>
+        <div>
+          <img src="https://github.com/vf-jara.png" className='rounded-full h-[150px]' />
+        </div>
+        <div className='py-5 text-center'>
+          <h1 className='text-2xl font-bold'>Vinicius Franco Jara</h1>
+          <p>Desenvolvedor Front-End<br />JavaScript | React | Next.js</p>
+        </div>
+        <div className='w-full flex flex-col gap-5 px-10'>
+          <a href='https://github.com/vf-jara' target="_blank" className='font-medium text-xl'>
+            <div className='bg-gray-800 text-white flex rounded-md'>
+              <div className=' px-5 py-3'>
+                <GithubLogo size={35} />
+              </div>
+              <div className=' pl-12 border-l px-5 py-4'>
+                <p>GitHub</p>
+              </div>
+            </div>
+          </a>
+          <a href='https://www.linkedin.com/in/vfjara21/' target="_blank" className='font-medium text-xl'>
+            <div className='bg-[#0a66c2] text-white flex rounded-md'>
+              <div className=' px-5 py-3'>
+                <LinkedinLogo size={35} />
+              </div>
+              <div className=' pl-12 border-l px-5 py-4'>
+                <p>LinkedIn</p>
+              </div>
+            </div>
+          </a>
+          <a href='mailto:viniciusfjara@gmail.com' target="_blank" className='font-medium text-xl'>
+            <div className='bg-[#c5221f] text-white flex rounded-md'>
+              <div className=' px-5 py-3'>
+                <Envelope size={35} />
+              </div>
+              <div className=' pl-12 border-l px-5 py-4'>
+                <p>E-mail</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
